242cba3d5a73e8275b734d8a47702baa04872c34
parhamfarnay <prhmfrny@gmail.com>
2024-09-28 01:22:08 +0330

    feat/context created and handled

---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,24 @@
+import React, { createContext, useEffect, useState } from "react";
+import api from "../services/config";
+const ProductContext = createContext();
+function ProductsProvider({ children }) {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setProducts(await api.get("/products"));
+      } catch (error) {
+        console.log(error.message);
+      }
+
+      fetchProducts();
+    };
+  }, []);
+  return (
+    <ProductContext.Provider value={products}>
+      {children}
+    </ProductContext.Provider>
+  );
+}
+
+export default ProductsProvider;
