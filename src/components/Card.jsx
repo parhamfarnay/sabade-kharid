2fe67100bc90676df6c2f04e0ae9ae7094fb5f47
parhamfarnay <prhmfrny@gmail.com>
2024-09-30 02:53:49 +0330

    feat/card component created

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,25 @@
+import { TbListDetails, TbShoppingBagCheck } from "react-icons/tb";
+import { Link } from "react-router-dom";
+import { shortenText } from "../helper/helper";
+import styles from "./Card.module.css";
+
 function Card({ data }) {
-  console.log(data);
-  return <div>Card</div>;
+  const { id, title, image, price } = data;
+  return (
+    <div className={styles.card}>
+      <img src={image} alt={title} />
+      <h3>{shortenText(title)}</h3>
+      <p>{price} $</p>
+      <div className={styles.actions}>
+        <Link to={`/products/${id}`}>
+          <TbListDetails />
+        </Link>
+        <button>
+          <TbShoppingBagCheck />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
