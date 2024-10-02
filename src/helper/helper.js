0f23414baf492cb2a89762898b7b0a5b2947d24e
parhamfarnay <prhmfrny@gmail.com>
2024-10-03 02:28:22 +0330

    feat/logics added to buttons of card component

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -47,6 +47,14 @@ const sumProducts = (products) => {
     .toFixed(2);
   return { itemsCounter, total };
 };
+const productQuantity = (state, id) => {
+  const index = state.selectedItems.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return 0;
+  } else {
+    return state.selectedItems[index].quantity;
+  }
+};
 
 export {
   shortenText,
@@ -55,4 +63,5 @@ export {
   createQueryObject,
   getInitialQuery,
   sumProducts,
+  productQuantity,
 };
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import "./global.css";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
