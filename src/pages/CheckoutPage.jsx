0c498841e9b19c8d2a1578c53fb51108307e377e
parhamfarnay <prhmfrny@gmail.com>
2024-10-03 04:41:32 +0330

    feat/sidebar created and styled

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,13 +1,24 @@
 import BasketCard from "../components/BasketCard";
+import BasketSidebar from "../components/BasketSidebar";
 import { useCart } from "../context/CartContext";
+import styles from "./CheckoutPage.module.css";
 function CheckoutPage() {
   const [state, dispatch] = useCart();
   const clickHandler = (type, payload) => {
     dispatch({ type, payload });
   };
+
+  if (!state.itemsCounter) {
+    return (
+      <div className={styles.container}>
+        <p>Empty</p>
+      </div>
+    );
+  }
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <BasketSidebar state={state} clickHandler={clickHandler} />
+      <div className={styles.products}>
         {state.selectedItems.map((product) => (
           <BasketCard
             key={product.id}
