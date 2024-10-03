548d0828f841fad5481e2e4334dc7307ffd4bf79
parhamfarnay <prhmfrny@gmail.com>
2024-10-03 03:30:20 +0330

    feat/details page completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,21 @@ import CheckoutPage from "./pages/CheckoutPage";
 import PageNotFound from "./pages/404";
 import ProductsProvider from "./context/ProductContext";
 import CartProvider from "./context/CartContext";
+import Layout from "./layout/layout";
 
 function App() {
   return (
     <CartProvider>
       <ProductsProvider>
-        <Routes>
-          <Route index element={<Navigate to="/products" replace />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="/products/:id" element={<DetailsPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/*" element={<PageNotFound />} />
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route index element={<Navigate to="/products" replace />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/products/:id" element={<DetailsPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/*" element={<PageNotFound />} />
+          </Routes>
+        </Layout>
       </ProductsProvider>
     </CartProvider>
   );
